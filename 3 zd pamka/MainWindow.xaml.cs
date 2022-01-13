63da8d7ade207ec2ebe56de2186950b6e02f02b5
Александр Иншаков <a.inshakov84@gmail.com>
2022-01-13 15:28:57 +0300

    Добавьте файлы проекта.

---
+++ 3 zd pamka/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _3_zd_pamka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+                textBox.FontFamily = new FontFamily(fontName);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == null)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                textBox.TextDecorations = null;
+            }
+        }
+
+       
+    }
+}
